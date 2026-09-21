628039d95e356d22781a93432a5bd808f99e04a1
Chaereve <148707259+Chaereve@users.noreply.github.com>
2026-09-21 21:49:55 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from "./App.tsx";
 import Background from './components/Background'
 import Toaster from './components/Toaster'
 import { I18nProvider } from './lib/i18n.jsx'
